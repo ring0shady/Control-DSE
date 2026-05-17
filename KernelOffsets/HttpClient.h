7ae9d85081560585a7927223c086dc5152d88323
Hossam Ayman Shady <hackerthe250@gmail.com>
2026-05-17 13:48:39 +0300

    Delete KernelOffsets directory

--- KernelOffsets/HttpClient.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-BOOL HttpsDownloadFullFile(LPCWSTR domain, LPCWSTR uri, PBYTE* output, SIZE_T* output_size);
--- KernelOffsets/PdbParser.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <Windows.h>
-
-PVOID extractGuidFromPdb(LPWSTR filepath);
